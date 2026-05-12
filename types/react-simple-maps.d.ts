f884981364799a24fa876c3fe40270f1197732fc
Bisrategebriel <bisrate22@gmail.com>
2026-05-12 22:39:05 +0300

    feat: add types package

---
+++ types/react-simple-maps.d.ts
@@ -0,0 +1,74 @@
+declare module "react-simple-maps" {
+  import * as React from "react";
+
+  export interface ComposableMapProps {
+    projection?: string | Function;
+    projectionConfig?: any;
+    width?: number;
+    height?: number;
+    style?: React.CSSProperties;
+    className?: string;
+    children?: React.ReactNode;
+  }
+
+  export interface GeographiesProps {
+    geography?: string | object | string[];
+    children?: (data: { geographies: any[] }) => React.ReactNode;
+    parseGeographies?: (geos: any[]) => any[];
+  }
+
+  export interface GeographyProps {
+    geography?: any;
+    onMouseEnter?: (event: React.MouseEvent) => void;
+    onMouseLeave?: (event: React.MouseEvent) => void;
+    onMouseDown?: (event: React.MouseEvent) => void;
+    onMouseUp?: (event: React.MouseEvent) => void;
+    onClick?: (event: React.MouseEvent) => void;
+    onFocus?: (event: React.FocusEvent) => void;
+    onBlur?: (event: React.FocusEvent) => void;
+    style?: {
+      default?: React.CSSProperties;
+      hover?: React.CSSProperties;
+      pressed?: React.CSSProperties;
+    };
+    className?: string;
+    fill?: string;
+    stroke?: string;
+    strokeWidth?: number;
+  }
+
+  export interface MarkerProps {
+    coordinates: [number, number];
+    children?: React.ReactNode;
+    onMouseEnter?: (event: React.MouseEvent) => void;
+    onMouseLeave?: (event: React.MouseEvent) => void;
+    onMouseDown?: (event: React.MouseEvent) => void;
+    onMouseUp?: (event: React.MouseEvent) => void;
+    onClick?: (event: React.MouseEvent) => void;
+    onFocus?: (event: React.FocusEvent) => void;
+    onBlur?: (event: React.FocusEvent) => void;
+    style?: {
+      default?: React.CSSProperties;
+      hover?: React.CSSProperties;
+      pressed?: React.CSSProperties;
+    };
+    className?: string;
+  }
+
+  export interface LineProps {
+    from: [number, number];
+    to: [number, number];
+    stroke?: string;
+    strokeWidth?: number;
+    strokeLinecap?: "butt" | "round" | "square" | "inherit";
+    className?: string;
+    style?: React.CSSProperties;
+  }
+
+  export const ComposableMap: React.FC<ComposableMapProps>;
+  export const Geographies: React.FC<GeographiesProps>;
+  export const Geography: React.FC<GeographyProps>;
+  export const Marker: React.FC<MarkerProps>;
+  export const Line: React.FC<LineProps>;
+  export const ZoomableGroup: React.FC<any>;
+}
